5f5b3091237bb4be7e50f4972de9c5ad47eaad53
Hakan <76572659+hakanakkuss@users.noreply.github.com>
2023-02-12 18:01:47 +0300

    upcoming page
    completed upcoming page

--- Movieflix/Controllers/SearchViewController.swift
+++ Movieflix/Controllers/SearchViewController.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let discoverTableView: UITableView = {
+       let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
         view.backgroundColor = .systemBackground
+        view.addSubview(discoverTableView)
         
+        discoverTableView.delegate = self
+        discoverTableView.dataSource = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTableView.frame = view.bounds
     }
     
 
 }
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
---
+++ Movieflix/ViewModel/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Movieflix
+//
+//  Created by Macbook Pro on 12.02.2023.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
