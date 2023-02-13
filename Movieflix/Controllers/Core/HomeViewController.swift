4fc691258f8d31bfb7d7f726a2a9434a5a9211a3
Hakan <76572659+hakanakkuss@users.noreply.github.com>
2023-02-13 15:27:06 +0300

    headerView
    random headerView

--- Movieflix/Controllers/Core/HomeViewController.swift
+++ Movieflix/Controllers/Core/HomeViewController.swift
@@ -18,6 +18,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie : Title?
+    private var headerView : HeroHeaderUIView?
+    
     let sectionTitles : [String] = ["Trending Movies", "Popular", "Trending TV", "Upcoming Movies", "Top Rated"]
     
     private let homeFeedTable : UITableView = {
@@ -37,10 +40,24 @@ class HomeViewController: UIViewController {
         homeFeedTable.dataSource = self
         
         configureNavBar()
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
-        
+        configureHeroHeaderView()
 
+    }
+    
+    ///Bu fonksiyon en üstte görünen fotoğrafı random olarak ayarlar.
+    private func configureHeroHeaderView(){
+        APICaller.shared.getTrendingMovies { result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self.randomTrendingMovie = selectedTitle //RandomTrendingMovie'i en üstte headerView ile birlikte tanımladık.
+                self.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case . failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavBar() {
